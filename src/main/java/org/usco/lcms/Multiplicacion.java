3635181c89a04e9019131cf2e3330eabaea1282e
juancastro <juan.castro.co@gmail.com>
2016-05-27 14:07:10 -0500

    cambio 03

---
+++ src/main/java/org/usco/lcms/Multiplicacion.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.usco.lcms;
+
+/**
+ * @author juancastro
+ *
+ */
+public class Multiplicacion {
+	/**
+	 * 
+	 * @param n1
+	 * @param n2
+	 * @return double la multiplicacion de n1 * n2
+	 */
+	public double getMultiplicacion(double n1, double n2){
+		return n1 * n2;
+	}
+
+}
