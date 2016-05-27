fd9b7bf1e4caae577ffe5d522a57c5f64c8fafc4
dacru-lcms <dacru-lcms@dacrulcms-HP-ProBook-440-G3>
2016-05-27 13:57:53 -0500

    cambio 02

--- src/main/java/org/usco/lcms/Suma.java
+++ src/main/java/org/usco/lcms/Suma.java
@@ -5,7 +5,7 @@ package org.usco.lcms;
 
 /**
  * @author juancastro
- * @version 1.0 27.05.2016
+ * @version 2.0 27.05.2016
  */
 public class Suma {
 	private double numero1;
@@ -72,5 +72,6 @@ public class Suma {
 	public double aumentar(double n1){
 		return n1 + 1;
 	}
+	
 	
 }
